345ece39cc572681a7c365c11b4a843c57904d2e
Javitodev <javito.dev@gmail.com>
2024-04-22 12:18:39 -0400

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce78eab18e6ddf95f27e1b590959a764'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '413bc478-85ee-4619-b14e-53ff215476d3'
+};
